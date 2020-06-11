474840c92b16be7f027c42092d918f07894624c5
Joshua Forman <cyborgforman@gmail.com>
2020-06-11 14:06:25 -0700

    create searchbar that makes api calls to omdb

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import "./App.css"
+import React, { useState } from 'react';
+import Search from './Search';
+import "../css/App.css";
+import axios from 'axios';
 
 
-function App() {
+function App(API_KEY) {
+  const [state, setState] = useState({
+    searchQuery: '',
+    results: [],
+    selected: {}
+  });
 
+  const apiurl = `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_API_KEY}`;
 
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      axios(apiurl + '&s=' + state.searchQuery).then((data) => {
+        console.log(data);
+      } );
+    }
+  }
+
+  const handleInput = (e) => {
+    let searchQuery = e.target.value;
+    setState(prevState => {
+      return { ...prevState, searchQuery: searchQuery }
+    });
+  }
+  
   return (
     <div className='app'>
-      <div>
-        hello world
-      </div>
-
+      <header>
+        <h1>hello world</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+      </main>
     </div>
   );
 }
